04767d2c9470f16e58f77408608ae984d46a12b6
ESchulstad <eschulstad@gmail.com>
2016-09-23 16:03:22 -0400

    Added Classes

---
+++ BankAccount/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public abstract class Account
+    {
+
+        //fields
+        protected string nameOnAccount = "Lebron James";
+
+
+        //properties
+
+        //methods
+
+        public abstract int Deposit(int money);
+
+
+        public abstract int Withdrawal(int money);
+        
+    }
+}
---
+++ BankAccount/SavingsAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class SavingsAccount : Account
+    {
+        public override int Deposit(int money)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Withdrawal(int money)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
