0addb44695622fea951413ce7693a03a476b33c0
ESchulstad <eschulstad@gmail.com>
2016-09-25 00:16:38 -0400

    Added Stream Writer

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -11,19 +11,39 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+
+            StreamWriter accountReserve = new StreamWriter("ReserveAccount.txt");
+            StreamWriter accountSavings = new StreamWriter("SavingsAccount.txt");
+            StreamWriter accountChecking = new StreamWriter("CheckingAccount.txt");
+
             //ask for name
 
             Console.WriteLine("Please enter your name for your password.");
             string holderName = Console.ReadLine();
-
+            
             Console.Clear();
 
             SavingsAccount savings = new SavingsAccount(holderName);
             CheckingAccount checking = new CheckingAccount(holderName);
             ReserveAccount reserve = new ReserveAccount(holderName);
-            
 
-            while(true)
+
+            accountChecking.WriteLine("Account Holder: " + holderName);
+            accountChecking.WriteLine("Account Number: " + checking.AccountNumber);
+            accountChecking.WriteLine("Account Type: Checking Account");
+            accountChecking.WriteLine("Account Balance: " + checking.AccountBalance);
+
+            accountSavings.WriteLine("Account Holder: " + holderName);
+            accountSavings.WriteLine("Account Number: " + savings.AccountNumber);
+            accountSavings.WriteLine("Account Type: Savings Account");
+            accountSavings.WriteLine("Account Balance: " + savings.AccountBalance);
+
+            accountReserve.WriteLine("Account Holder: " + holderName);
+            accountReserve.WriteLine("Account Number: " + reserve.AccountNumber);
+            accountReserve.WriteLine("Account Type: Reserve Account");
+            accountReserve.WriteLine("Account Balance: " + reserve.AccountBalance);
+
+            while (true)
             {
                 //menu
                 Console.WriteLine("Welcome! What would you like to do?");
@@ -75,17 +95,27 @@ namespace BankAccount
                             case 1:
                                 checking.Deposit(deposit);
                                 Console.WriteLine("The new balance is " + checking.AccountBalance);
+                                accountChecking.WriteLine("+ " + deposit + " " + DateTime.Now);
+                                accountChecking.WriteLine("Account Balance: " + checking.AccountBalance);
                                 break;
 
                             case 2:
                                 savings.Deposit(deposit);
                                 Console.WriteLine("The new balance is " + savings.AccountBalance);
+                                accountSavings.WriteLine("+ " + deposit + " " + DateTime.Now);
+                                accountSavings.WriteLine("Account Balance: " + savings.AccountBalance);
                                 break;
 
                             case 3:
                                 reserve.Deposit(deposit);
                                 Console.WriteLine("The new balance is " + reserve.AccountBalance);
+                                accountReserve.WriteLine("+ " + deposit + " " + DateTime.Now);
+                                accountReserve.WriteLine("Account Balance: " + reserve.AccountBalance); 
                                 break;
+
+                            default:
+                                break;
+
                         }
                         break;
 
@@ -106,30 +136,35 @@ namespace BankAccount
                             case 1:
                                 checking.Withdrawal(withdrawal);
                                 Console.WriteLine("The new balance is " + checking.AccountBalance);
+                                accountChecking.WriteLine("- " + withdrawal + " " + DateTime.Now);
+                                accountChecking.WriteLine("Account Balance: " + checking.AccountBalance);
                                 break;
 
                             case 2:
                                 savings.Withdrawal(withdrawal);
                                 Console.WriteLine("The new balance is " + savings.AccountBalance);
+                                accountSavings.WriteLine("- " + withdrawal + " " + DateTime.Now);
+                                accountSavings.WriteLine("Account Balance: " + savings.AccountBalance);
                                 break;
 
                             case 3:
                                 reserve.Withdrawal(withdrawal);
                                 Console.WriteLine("The new balance is " + reserve.AccountBalance);
+                                accountReserve.WriteLine("- " + withdrawal + " " + DateTime.Now);
+                                accountReserve.WriteLine("Account Balance: " + reserve.AccountBalance);
                                 break;
                         }
                         break;
                        
                     case 7:
-                        Quit();
                         break;
 
                     default:
-                        Quit();
                         break;
                        
                 }
 
+               
                 Console.WriteLine("Would you like to do something else? Y or N?");
                 string yesOrNo = Console.ReadLine();
                 if(yesOrNo.ToLower()=="y")
@@ -139,10 +174,16 @@ namespace BankAccount
                 else
                 {
                     Console.Clear();
-                    Quit();
+                    break;
                 }
 
             }
+
+            accountReserve.Close();
+            accountSavings.Close();
+            accountChecking.Close();
+
+            Quit();
         }
 
 
